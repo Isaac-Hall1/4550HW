5c3daeae87e1f35c1c05c411c88d616435a2eed9
Isaac Hall <106003051+Isaac-Hall1@users.noreply.github.com>
2025-01-14 14:57:52 -0700

    made code clearer

--- main.py
+++ main.py
@@ -12,47 +12,59 @@ import json
 
 app = FastAPI()
 
-
-def getData():
+#gets all books
+def getDataBooks():
     with open('data.json', 'r') as file:
         data = json.load(file)
-    return data
+    return data['books']
 
+#gets all authors
+def getDataAuthors():
+    with open('data.json', 'r') as file:
+        data = json.load(file)
+    return data['authors']
+
+#returns sorted data
+def sortById(list):
+    list.sort(key=lambda x:x['id'])
+    return list
+
+#returns all books
 @app.get("/books")
 def getBooks():
-    data = getData()
-    books = data['books'] 
-    books.sort(key=lambda x:x['id'])
-    return books
+    books = getDataBooks()
+    return sortById(books)
+
+#returns specified book
 @app.get("/books/{book_id}")
 def getBookById(book_id:int):
-    data = getData()
-    books = data['books']
+    books = getDataBooks()
     for book in books:
         if book['id'] == book_id:
             return book
     return {"message": "Book not found"}
+
+#returns all authors
 @app.get("/authors")
 def getAuthors():
-    data = getData()
-    authors = data['authors']
-    authors.sort(key=lambda x:x['id'])
-    return authors
+    authors = getDataAuthors()
+    return sortById(authors)
+
+#returns specified author
 @app.get("/authors/{author_id}")
 def getAuthorById(author_id:int):
-    data = getData()
-    authors = data['authors']
+    authors = getDataAuthors()
     for auth in authors:
         if auth['id'] == author_id:
             return auth
     return {"message": "author not found"}
+
+#returns books from specified author
 @app.get("/authors/{author_id}/books")
 def getAuthorsBooks(author_id:int):
-    data = getData()
-    books = data['books']
+    books = getDataBooks()
     retBooks = []
     for book in books:
         if book['author_id'] == author_id:
             retBooks.append(book)
-    retBooks.sort(key=lambda x:x['id'])
-    return retBooks
+    return sortById(retBooks)
